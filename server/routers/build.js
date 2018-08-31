6f2c2bbc4d5946a7d169ebcffc13a303b8e38f24
Nfer Zhuang <nfer.zhuang@gmail.com>
2018-08-31 10:57:14 +0800

    fix some eslint error

--- server/routers/build.js
+++ server/routers/build.js
@@ -1,186 +1,179 @@
-'use strict';
-
-var build = require('../factory/build.js');
-var parseRes = require('../utils/parseRes.js');
-var actDao = require('../db/dao/activies');
-var utils = require('../utils/utils.js');
-var zip = require('express-zip');
-
+const build = require('../factory/build.js');
+const parseRes = require('../utils/parseRes.js');
+const actDao = require('../db/dao/activies');
+const utils = require('../utils/utils.js');
 
 /**
  *  转化为前端可见的图片地址
  */
 function dueDataImage(data) {
-    var pages = data.pages;
-    for (var i = 0; i < pages.length; i++) {
-        var page = pages[i];
-        page.burl = page.burl.replace('../images', '/upload');
-        var items = page.items;
-        for (var j = 0; j < items.length; j++) {
-            var item = items[j];
-            if (item.imgUrl) {
-                if (item.imgUrl.indexOf('default.png') >= 0) {
-                    item.imgUrl = item.imgUrl.replace('images', '/client/images');
-                } else {
-                    item.imgUrl = item.imgUrl.replace('images', '/upload');
-                }
-            }
-
-            //去掉多余的#
-            if(item.textStyle && item.textStyle.color) {
-                item.textStyle.color = item.textStyle.color.replace('#', '');
-            }
+  const [pages] = data;
+  for (let i = 0; i < pages.length; i += 1) {
+    const page = pages[i];
+    page.burl = page.burl.replace('../images', '/upload');
+    const [items] = page;
+    for (let j = 0; j < items.length; j += 1) {
+      const item = items[j];
+      if (item.imgUrl) {
+        if (item.imgUrl.indexOf('default.png') >= 0) {
+          item.imgUrl = item.imgUrl.replace('images', '/client/images');
+        } else {
+          item.imgUrl = item.imgUrl.replace('images', '/upload');
         }
+      }
+
+      // 去掉多余的#
+      if (item.textStyle && item.textStyle.color) {
+        item.textStyle.color = item.textStyle.color.replace('#', '');
+      }
     }
+  }
 
-    return data;
-};
+  return data;
+}
 
-module.exports = function(app) {
-
-    /**
+module.exports = function route(app) {
+  /**
      *  构建预览
      */
-    app.post('/build', function(req, res) {
+  app.post('/build', (req, res) => {
+    // 获取参数
+    const data = JSON.parse(req.body.config);
 
-        //获取参数
-        var data = JSON.parse(req.body.config);
+    // 根据不同用户生成不同的临时目录
+    const target = `temp/${req.sessionID}`;
+    build(data, (viewPath) => {
+      res.send(parseRes.success({
+        viewPath,
+      }));
+    }, target);
+  });
 
-        //根据不同用户生成不同的临时目录
-        var target = 'temp/' + req.sessionID;
-        build(data, function(viewPath) {
-            res.send(parseRes.success({
-                viewPath: viewPath
-            }));
-        }, target);
-    });
-
-    /**
+  /**
      *  保存
      */
-    app.post('/save', function(req, res) {
-        //获取参数
-        var data = JSON.parse(req.body.config);
-        //保存
-        actDao.saveOrUpdate(data, function(result) {
-            if (result.code !== 0) {
-                res.send(parseRes.error(result));
-            } else {
+  app.post('/save', (req, res) => {
+    // 获取参数
+    const data = JSON.parse(req.body.config);
+    // 保存
+    actDao.saveOrUpdate(data, (result) => {
+      if (result.code !== 0) {
+        res.send(parseRes.error(result));
+      } else {
+        // 以id为目录生成项目代码
+        const target = `projects/${result.id}`;
 
-                //以id为目录生成项目代码
-                var target = 'projects/' + result.id;
-
-                build(data, function(viewPath) {
-                    res.send(parseRes.success({
-                        id: result.id
-                    }));
-                }, target);
-            }
-        });
+        build(data, () => {
+          res.send(parseRes.success({
+            id: result.id,
+          }));
+        }, target);
+      }
     });
+  });
 
-    /**
+  /**
      *
      * 根据id编辑
-     * 
+     *
      */
-    app.get('/edit', function(req, res) {
-        var id = req.param('id');
-        actDao.findById(id, function(data) {
-            res.render('edit.html', {
-                slug: 'edit',
-                data: dueDataImage(data)
-            });
-        });
+  app.get('/edit', (req, res) => {
+    const id = req.param('id');
+    actDao.findById(id, (data) => {
+      res.render('edit.html', {
+        slug: 'edit',
+        data: dueDataImage(data),
+      });
     });
+  });
 
-    /**
+  /**
      * 查询列表
      */
-    app.get('/findList', function(req, res) {
-        var domain = process.host;
-        actDao.findList(function(data) {
-            res.send(parseRes.success({
-                domain: domain,
-                editUrl: '/edit',
-                deleteUrl: '/delete',
-                addUrl: '/add',
-                list: data
-            }));
-        });
+  app.get('/findList', (req, res) => {
+    const domain = process.host;
+    actDao.findList((data) => {
+      res.send(parseRes.success({
+        domain,
+        editUrl: '/edit',
+        deleteUrl: '/delete',
+        addUrl: '/add',
+        list: data,
+      }));
     });
+  });
 
-    /**
+  /**
      *  主页
      */
-    app.get('/home', function(req, res) {
-        var domain = process.host;
-        actDao.findList(function(data) {
-            res.render('home.html', {
-                slug: 'home',
-                data: data,
-                domain: domain
-            });
-        });
+  app.get('/home', (req, res) => {
+    const domain = process.host;
+    actDao.findList((data) => {
+      res.render('home.html', {
+        slug: 'home',
+        data,
+        domain,
+      });
     });
+  });
 
-    /**
+  /**
      *  增加
      */
-    app.get('/add', function(req, res) {
-        res.render('edit.html', {
-            slug: 'edit'
-        });
+  app.get('/add', (req, res) => {
+    res.render('edit.html', {
+      slug: 'edit',
     });
+  });
 
-    /**
+  /**
      *  show-h5
      */
-    app.get('/show-h5.html', function(req, res) {
-        res.render('show-h5.html', {
-            slug: 'show-h5'
-        });
+  app.get('/show-h5.html', (req, res) => {
+    res.render('show-h5.html', {
+      slug: 'show-h5',
     });
+  });
 
-    /**
+  /**
      *  下载
      */
-    app.get('/download', function(req, res) {
-        var id = req.param('id');
-        var files = utils.getAllFiles(process.rootPath + '/output/projects/' + id);
+  app.get('/download', (req, res) => {
+    const id = req.param('id');
+    const files = utils.getAllFiles(`${process.rootPath}/output/projects/${id}`);
 
-        var outFiles = [];
-        files.forEach(function(item) {
-            outFiles.push({
-                path: item,
-                name: item.slice(item.indexOf(id))
-            });
-        });
-
-        console.log(outFiles);
-        res.zip(outFiles, id + '.zip', function(err, d) {
-            console.log('---- send zip callback -----');
-            console.log(err);
-            console.log(d)
-            res.end();
-        });
+    const outFiles = [];
+    files.forEach((item) => {
+      outFiles.push({
+        path: item,
+        name: item.slice(item.indexOf(id)),
+      });
     });
 
-    /**
+    console.log(outFiles);
+    res.zip(outFiles, `${id}.zip`, (err, d) => {
+      console.log('---- send zip callback -----');
+      console.log(err);
+      console.log(d);
+      res.end();
+    });
+  });
+
+  /**
      *  根据id删除
      */
-    app.get('/delete', function(req, res) {
-        var id = req.param('id');
-        actDao.deleteById(id, function(data) {
-            if(data.code != '000000') {
-                res.send(parseRes.error(data));
-            } else {
-                res.send(parseRes.success({}));
-            }
-        });
+  app.get('/delete', (req, res) => {
+    const id = req.param('id');
+    actDao.deleteById(id, (data) => {
+      if (data.code !== '000000') {
+        res.send(parseRes.error(data));
+      } else {
+        res.send(parseRes.success({}));
+      }
     });
+  });
 
-    app.get('/', function(req, res) {
-        res.redirect('/home');
-    });
-}
+  app.get('/', (req, res) => {
+    res.redirect('/home');
+  });
+};
--- server/routers/index.js
+++ server/routers/index.js
@@ -1,55 +1,20 @@
-// 公用路由器
-var express = require('express');
-var router = express.Router();
-var utils = require('../utils/utils');
-
-
-// 获取页面路由器
-// var list = [];
-// var files = utils.getAllFiles(process.rootPath + '/server/views', /^((?!_inc).)*$/);
-// for (var key in files) {
-//     list.push(files[key].replace(process.rootPath + '/server/views/', ''));
-// }
-
+const utils = require('../utils/utils');
 
 // 控制器路由表
-var actionList = [];
-files = utils.getAllFiles(process.rootPath + '/server/routers');
-for (var key in files) {
-    if(files[key].indexOf('index.js') < 0) {
-        actionList.push(files[key].replace(process.rootPath + '/server/routers/', ''));
-    }
-}
-
+const actionList = [];
+const files = utils.getAllFiles(`${process.rootPath}/server/routers`);
+files.forEach((item) => {
+  if (item.indexOf('index.js') < 0) {
+    actionList.push(item.replace(`${process.rootPath}/server/routers/`, ''));
+  }
+});
 
 module.exports = (app) => {
-
-    // 循环配置页面路由表
-    // Array.from(list, (page) => {
-
-    //     var key = '/' + page;
-
-    //     app.use(router.get(key, function(req, res) {
-    //         res.render(page, {
-    //             slug: key.replace('/', '').replace('.html', '')
-    //         });
-    //     }));
-
-    //     console.log('http://' + utils.getIp() + ':' + process.port + key);
-    //     return page;
-    // });
-
-    // console.log('访问上面路径，访问对应页面');
-
-
-    //循环配置控制器路由表
-    Array.from(actionList, (page) => {
-        require('./' + page)(app);
-        return page;
-    });
-
+  // 循环配置控制器路由表
+  /* eslint-disable global-require */
+  /* eslint-disable import/no-dynamic-require */
+  Array.from(actionList, (page) => {
+    require(`./${page}`)(app);
+    return page;
+  });
 };
-
-
-
-
