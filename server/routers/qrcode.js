155122841db712669a4334ca5ddb6d15d1507da7
Nfer Zhuang <nfer.zhuang@gmail.com>
2018-09-08 21:24:15 +0800

    both support /cgi/qrcode and /qrcode

--- server/routers/qrcode.js
+++ server/routers/qrcode.js
@@ -1,10 +1,13 @@
 const qr = require('qr-image');
 
+function genQrcode(req, res) {
+  const text = req.query.url;
+  const img = qr.image(text, { size: 10 });
+  res.set('Content-Type', 'image/png');
+  img.pipe(res);
+}
+
 module.exports = function upload(app) {
-  app.get('/qrcode', (req, res) => {
-    const text = req.query.url;
-    const img = qr.image(text, { size: 10 });
-    res.set('Content-Type', 'image/png');
-    img.pipe(res);
-  });
+  app.get('/qrcode', genQrcode);
+  app.get('/cgi/qrcode', genQrcode);
 };
